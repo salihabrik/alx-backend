111b5a9fbd19ef5a698ce106d109c852bcd0f65a
salihabrik <briksaliha@gmail.com>
2024-01-30 23:27:03 +0100

    BasicCache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" BasicCache module
+"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """ BasicCache inherits from BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
