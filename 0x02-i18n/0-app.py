caaa5075d273bc98712bdb70d71f2245fcd421de
salihabrik <briksaliha@gmail.com>
2024-02-06 20:50:35 +0100

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 
 from flask import Flask, render_template
@@ -9,3 +9,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('0-index.html')
+"""A Basic Flask app.
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+        """The home/index page.
+            """
+                return render_template('0-index.html')
+
+
+            if __name__ == '__main__':
+                    app.run(host='0.0.0.0', port=5000)
+
+
